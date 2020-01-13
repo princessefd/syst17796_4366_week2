d29a6f4d5797c5396d3e29220e843db9151977ab
emilydiano <dianoe@sheridancollege.ca>
2020-01-13 14:46:36 -0500

    initial commit

---
+++ src/software/week2/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package software.week2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author emilydiano
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Student[] s = new Student[5];
+        
+        Scanner keysIn = new Scanner (System.in);
+        
+        for(int i=0; i<s.length; i++) {
+            
+            s[i] = new Student();
+            
+            System.out.println("Enter id: ");
+            s[i].setId(keysIn.nextInt());
+            
+            System.out.println("Enter name: ");
+            s[i].setName(keysIn.next());
+        }
+    }
+    
+}
